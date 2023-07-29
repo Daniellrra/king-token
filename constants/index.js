10191eec83ebbbcffa42474f2c0e50454ffeb517
Daniellrra <134928136+Daniellrra@users.noreply.github.com>
2023-07-29 13:30:21 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbc87f87e45d8d376fbe270304dbe44576fd2360c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
